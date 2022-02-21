fb77d20131e82b3147bfe7d307da6f3e731ff415
Ole Ildsgaard Hougaard (OIH) | VIA <OIH@VIAUC.DK>
2022-02-21 14:48:31 +0100

    After session 4

--- session4/src/main/java/dk/via/mvvm/view/CreateAccountViewController.java
+++ session4/src/main/java/dk/via/mvvm/view/CreateAccountViewController.java
@@ -1,22 +1,31 @@
 package dk.via.mvvm.view;
 
+import dk.via.mvvm.viewmodel.CreateAccountViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 
 public class CreateAccountViewController {
     @FXML private TextField username;
     @FXML private TextField password;
     @FXML private TextField email;
     @FXML private Label errorLabel;
+    private ViewHandler viewHandler;
+    private CreateAccountViewModel viewModel;
+    private Region root;
 
     @FXML
     protected void createButtonPressed() {
+        boolean success = viewModel.createUser();
+        if (success) {
+            viewHandler.openView(ViewHandler.MESSAGE);
+        }
     }
 
     @FXML
     protected void cancelButtonPressed() {
-
+        viewHandler.closeView();
     }
 
     @FXML
@@ -32,5 +41,24 @@ public class CreateAccountViewController {
     @FXML
     public void submitEmail() {
         createButtonPressed();
+    }
+
+    public void init(ViewHandler viewHandler, CreateAccountViewModel createAccountViewModel, Region root) {
+        this.viewHandler = viewHandler;
+        this.viewModel = createAccountViewModel;
+        this.root = root;
+
+        createAccountViewModel.bindUsername(username.textProperty());
+        createAccountViewModel.bindPassword(password.textProperty());
+        createAccountViewModel.bindEmail(email.textProperty());
+        createAccountViewModel.bindError(errorLabel.textProperty());
+    }
+
+    public Region getRoot() {
+        return root;
+    }
+
+    public void reset() {
+        viewModel.reset();
     }
 }
