e545b76d38090fbfafd69ad9a3959459ae3b2deb
Ole Ildsgaard Hougaard (OIH) | VIA <OIH@VIAUC.DK>
2022-03-18 07:22:32 +0100

    Removed generated files from git

--- session7/src/dk/via/echo/EchoClient.java
+++ session7/src/dk/via/echo/EchoClient.java
@@ -1,30 +1,9 @@
 package dk.via.echo;
 
-import java.io.*;
-import java.net.Socket;
+import java.io.IOException;
 
-public class EchoClient {
-    private final Socket socket;
-    private final PrintWriter writer;
-    private final BufferedReader reader;
+public interface EchoClient {
+    String echo(String message) throws IOException;
 
-    public EchoClient(String host, int port) throws IOException {
-        socket = new Socket(host, port);
-        OutputStream outputStream = socket.getOutputStream();
-        writer = new PrintWriter(outputStream);
-        InputStream inputStream = socket.getInputStream();
-        reader = new BufferedReader(new InputStreamReader(inputStream));
-    }
-
-    public String echo(String message) throws IOException {
-        writer.println(message);
-        writer.flush();
-        return reader.readLine();
-    }
-
-    public void close() throws IOException {
-        writer.println("exit");
-        writer.flush();
-        socket.close();
-    }
+    void close() throws IOException;
 }
